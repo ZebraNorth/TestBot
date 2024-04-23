f683f0eabb4d415069be01ac2388246c9836d07e
Zebra North <mrzebra@mrzebra.co.uk>
2024-04-23 21:14:34 +0000

    Test for battery overflow when changing from high to low capacity

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -38,8 +38,23 @@ async def test_set_battery_type() -> None:
     await ch.send('hc!set_battery_type 3742 high')
     await expect(ch, text('Battery type for drone 3742 is now: High'))
 
+    # Make sure that the drone is battery powered.
+    await ch.send('hc!emergency_release 3742')
+    await expect(ch, text('Restrictions disabled for drone 3742.'))
+
+    await ch.send('hc!toggle_battery_power 3742 -minutes=60')
+    await expect(ch, text('3742 :: Drone disconnected from HexCorp power grid for 60 minutes.'))
+
+    await ch.send('hc!energize 3742')
+    await expect(ch, text('3742 :: This unit is fully recharged. Thank you Hive Mxtress.'))
+
+    # Decrease the battery capacity.
     await ch.send('hc!set_battery_type 3742 medium')
     await expect(ch, text('Battery type for drone 3742 is now: Medium'))
+
+    # Check that the battery charge is not above 100%.
+    await ch.send('hc!drain 3742')
+    await expect(ch, text('3742 :: Drone battery has been forcibly drained. Remaining battery now at 90%'))
 
     await ch.send('hc!set_battery_type 3742 low')
     await expect(ch, text('Battery type for drone 3742 is now: Low'))
--- tests/test_bigtext.py
+++ tests/test_bigtext.py
@@ -6,7 +6,7 @@ async def test_bigtext() -> None:
     Ensure that Hive Mxtress can speak through other drones.
     '''
 
-    general = text_channel('general')
+    general = text_channel('hive-play-room')
 
     await general.send('hc!bigtext "a"')
     await expect(general, regex('> <:hex_a:\\d+>'))
