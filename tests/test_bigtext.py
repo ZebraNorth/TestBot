497747691e37464213203adbfe9be0eab74c240c
Zebra North <mrzebra@mrzebra.co.uk>
2024-05-12 17:20:36 +0000

    bigtext does not work in hive rooms.

--- tests/test_bigtext.py
+++ tests/test_bigtext.py
@@ -3,10 +3,10 @@ from testbot import expect, regex, text_channel
 
 async def test_bigtext() -> None:
     '''
-    Ensure that Hive Mxtress can speak through other drones.
+    Ensure that big text can be generated with emoticons.
     '''
 
-    general = text_channel('hive-play-room')
+    general = text_channel('general')
 
     await general.send('hc!bigtext "a"')
     await expect(general, regex('> <:hex_a:\\d+>'))
